eaede98115e60ab758ba506c4b15875f4cfc564a
Anna Smolskaya <anna31rus@mail.ru>
2022-08-29 19:54:57 +0300

    corrected accordig with flake8

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -3,12 +3,13 @@ from brain_games.cli import welcome_user
 
 
 def greet():
-    print(f'Welcome to the Brain Games!')
+    print('Welcome to the Brain Games!')
+
 
 def main():
     greet()
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
