addfe1cea82341b4d9f2433238c92e1173bd6819
Anna Smolskaya <anna31rus@mail.ru>
2022-08-29 19:56:11 +0300

    corrected according with flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,14 +1,14 @@
 import prompt
 
+
 def welcome_user():
     who = prompt.string('May I have your name? ')
     print(f'Hello, {who}!')
 
+
 def main():
     welcome_user()
 
+
 if __name__ == '__main__':
     main()
-
-
-
