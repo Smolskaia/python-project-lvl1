2d4b724af344735517b9c26df1256d9ccc856fb8
Anna Smolskaya <anna31rus@mail.ru>
2022-08-30 19:00:24 +0300

    added greeting in brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
+import prompt
+from brain_games.scripts.brain_games import greet
+from brain_games.cli import welcome_user
 
 
+def main():
+    greet()
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
